86ee76492145ac0a1e2b1682bf7079031410c938
Brett <bretttucker@gmail.com>
2019-08-24 07:55:58 -0700

    Add initial project files

---
+++ src/main/kotlin/com/abnormallydriven/minecraftarchiver/App.kt
@@ -0,0 +1,13 @@
+package com.abnormallydriven.minecraftarchiver
+
+fun main(args: Array<String>) {
+    //project id
+    //google account file location
+    //minecraft folder location
+
+    if(args.size < 3){
+        System.err.println("Invalid program arguments. Please provide a project id, google service account file, and minecraft folder location.")
+        return
+    }
+
+}
